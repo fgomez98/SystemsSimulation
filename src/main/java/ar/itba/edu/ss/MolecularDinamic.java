ec8beaa92c01630cc3aae990e572e0fd7e489873
Fermin <fgomez@itba.edu.ar>
2020-03-29 01:06:25 -0300

    Data para graficos

---
+++ src/main/java/ar/itba/edu/ss/MolecularDinamic.java
@@ -0,0 +1,120 @@
+package ar.itba.edu.ss;
+
+import ar.itba.edu.ss.systems.BrownianMotion;
+import ar.itba.edu.ss.utils.CmdParserUtils;
+import org.kohsuke.args4j.Option;
+
+import java.io.IOException;
+
+public class MolecularDinamic {
+
+    @Option(name = "-Dn", usage = "Numero de particulas", forbids = {"-Ddinput", "Dsinput"})
+    private int n;
+
+    @Option(name = "-Dt", usage = "Tiempo a simular", required = true)
+    private int time;
+
+    @Option(name = "-Dsinput", usage = "Archivo estatico", forbids = {"-Dn"})
+    private String staticInputFilename;
+
+    @Option(name = "-Ddinput", usage = "Archivo dinamico", forbids = {"-Dn"})
+    private String dinamicInputFilename;
+
+    public MolecularDinamic() {
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getStaticInputFilename() {
+        return staticInputFilename;
+    }
+
+    public void setStaticInputFilename(String staticInputFilename) {
+        this.staticInputFilename = staticInputFilename;
+    }
+
+    public String getDinamicInputFilename() {
+        return dinamicInputFilename;
+    }
+
+    public void setDinamicInputFilename(String dinamicInputFilename) {
+        this.dinamicInputFilename = dinamicInputFilename;
+    }
+
+    public static void main(String args[]) {
+        MolecularDinamic md = new MolecularDinamic();
+        try {
+            CmdParserUtils.init(args, md);
+        } catch (IOException e) {
+            System.out.println("There was a problem reading the arguments");
+            System.exit(1);
+        }
+
+        md.start();
+    }
+
+    public void start() {
+        BrownianMotion brownianMotion = null;
+
+        if (staticInputFilename != null && dinamicInputFilename != null) {
+//
+////            TODO: read de particulas autonomas (hacer le metodo read generico para ambas)
+//
+//            List<MovingParticle> data = null;
+//            try {
+//                data = IOUtils.CSVReadMovingParticles(staticInputFilename, dinamicInputFilename);
+//            } catch (Exception e) {
+//                System.err.println("An error has been encountered while reading input file");
+//                System.exit(1);
+//            }
+//
+//        /*
+//            Necesitamos conocer el N y L pasado en el archivo estatico
+//         */
+//
+//            Scanner params = null;
+//            try {
+//                params = new Scanner(new File(staticInputFilename));
+//            } catch (FileNotFoundException e) {
+//                System.err.println("An error has been encountered while reading input file");
+//                System.exit(1);
+//            }
+//
+//            int n = params.nextInt(); // leemos el N pasado por el archivo estatico
+//            double l = params.nextDouble(); // leemos el L pasado por el archivo estatico
+//
+//            brownianMotion = new BrownianMotion(10);
+            return;
+        } else {
+            brownianMotion = new BrownianMotion(n);
+        }
+
+        System.out.println("Starting simulation...");
+
+        long startTime = System.currentTimeMillis();
+
+        try {
+            brownianMotion.simulate(time);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        long elapsedTime = System.currentTimeMillis() - startTime;
+
+        System.out.println("Time elapsed: " + ((double) elapsedTime / 1000));
+    }
+}
--- src/main/java/ar/itba/edu/ss/systems/BrownianMotion.java
+++ src/main/java/ar/itba/edu/ss/systems/BrownianMotion.java
@@ -3,13 +3,21 @@ package ar.itba.edu.ss.systems;
 import ar.itba.edu.ss.model.Event;
 import ar.itba.edu.ss.model.HardParticle;
 import ar.itba.edu.ss.utils.IOUtils;
+import javafx.util.Pair;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static ar.itba.edu.ss.utils.IOUtils.*;
 
 public class BrownianMotion {
+
+    private static long COLLISIONS = 0;
+    private static String PDF_COLLISIONS_FILENAME = "pdf-colisiones.txt";
+    private static String PDF_VELOCITY_THIRD_FILENAME = "pdf-velocidad-third.txt"; /* TODO: que onda lo del ultimo tercio ??*/
+    private static String PDF_VELOCITY_INITIAL_FILENAME = "pdf-velocidad-initial.txt"; /* TODO: que onda lo del ultimo tercio ??*/
+    private static String BIG_PARTICLE_TRAJECTORY_FILENAME = "big-particle-trajectory.txt";
 
     private Queue<Event> queue;
     private List<HardParticle> particles;
@@ -20,24 +28,39 @@ public class BrownianMotion {
     private final double M1 = 0.1;
     private final double R2 = 0.05;
     private final double M2 = 100;
+    private List<Double> collitionTimes;
+    /*
+     particula -> suma de modulo de velocidades para el ultimo tercio de la simulacion
+    Con esto luego calculamos el promedio
+    */
+    private Map<Long, Double> thirdIterationVelocity;
+    private Map<Long, Double> initialIterationVelocity;
+    private long thirdIterations = 0;
+    private List<Pair<Double, Double>> bigParticleTrayectory;
+    private HardParticle bigParticle;
 
-    private BrownianMotion() {
-
-    }
 
     public BrownianMotion(List<HardParticle> particles) {
+        this.bigParticle = particles.stream().max(Comparator.comparingDouble(HardParticle::getMass).thenComparingDouble(HardParticle::getRadius)).get();
         this.queue = new PriorityQueue<>();
         this.particles = particles;
         this.N = particles.size();
         this.M = (int) Math.ceil(L / 2 * Math.max(R1, R2) + 0);    // M optimo, criterio: L/M > radio interaccion + 2 * radioMax
+        this.collitionTimes = new LinkedList<>();
+        this.thirdIterationVelocity = new HashMap<>();
+        this.initialIterationVelocity = new HashMap<>();
+        this.bigParticleTrayectory = new LinkedList<>();
     }
 
     public BrownianMotion(int n) {
-        this();
+        this.collitionTimes = new LinkedList<>();
+        this.thirdIterationVelocity = new HashMap<>();
+        this.initialIterationVelocity = new HashMap<>();
+        this.bigParticleTrayectory = new LinkedList<>();
         this.queue = new PriorityQueue<>();
         this.N = n;
         this.M = (int) Math.ceil(L / 2 * Math.max(R1, R2) + 0); // M optimo, criterio: L/M > radio interaccion + 2 * radioMax
-        HardParticle particle = (HardParticle) new HardParticle.Builder()
+        this.bigParticle = (HardParticle) new HardParticle.Builder()
                 .withMass(M2)
                 .withRandomAngle(2 * Math.PI)
                 .withVelocity(0)
@@ -45,7 +68,7 @@ public class BrownianMotion {
                 .withRadius(R2)
                 .build();
         List<HardParticle> initialParticles = new LinkedList<>();
-        initialParticles.add(particle);
+        initialParticles.add(this.bigParticle);
         this.particles = HardParticle.generate(initialParticles, N - 1, (L - 2 * R2) + R2, R1, 2 * Math.PI, 0.1, M1);
         try {
             IOUtils.CSVWriteParticles(this.particles, BROWNIAN_MOTION_STATIC_FILENAME, BROWNIAN_MOTION_DINAMIC_FILENAME, this.particles.size(), L);
@@ -65,7 +88,7 @@ public class BrownianMotion {
         queue.offer(event);
     }
 
-    public void simulate(double simulationTime) {
+    public void simulate(double simulationTime) throws IOException {
         /*
             TODO: Cell index method para ver con cuales de mis vecinos estoy a distancia 0??, es decir me la puse
             El sistema es confinad por lo que no hay condicines de controno
@@ -77,6 +100,7 @@ public class BrownianMotion {
         int frame = 0;
         double time = 0;
         IOUtils.ovitoOutputParticles(BROWNIAN_MOTION_SIMULATION_FILENAME, particles, frame++, false);
+        saveVelocities(initialIterationVelocity);
 
         computeCollisions(time);
 
@@ -84,16 +108,26 @@ public class BrownianMotion {
             Event event = getNextEvent().get();
             /* Tiempo del el primer choque o incrementamos en 1 sino hay para redibujar el sistema */
             double tc = event.getTime();
-            double auxTime = tc - time;
+            double auxTime = tc - time; // --> tiempo entre colsiones
+            collitionTimes.add(auxTime);
             /*  Se evolucionan todas las partículas según sus ecuaciones de movimiento hasta tc */
             particles.forEach(p -> p.move(auxTime));
             /* Adelantamos el tiempo */
             time = tc;
+
+            if (time < ((1 / 3.0) * simulationTime)) { // último tercio de la simulación
+                saveVelocities(thirdIterationVelocity);
+                thirdIterations++;
+            }
+            saveBigParticlePosition(simulationTime, time);
+
             /* Collisionamos */
+            COLLISIONS++;
             collide(event, time);
             /* Output */
             IOUtils.ovitoOutputParticles(BROWNIAN_MOTION_SIMULATION_FILENAME, particles, frame++, true);
         }
+        outputCalculations(simulationTime);
     }
 
     private void collide(Event event, double currentTime) {
@@ -159,13 +193,62 @@ public class BrownianMotion {
         }
     }
 
+    private void outputCalculations(double simulationTime) throws IOException {
+        IOUtils.CSVWrite(PDF_COLLISIONS_FILENAME,
+                collitionTimes,
+                collitionTimes.size() / simulationTime + "\n\n",
+                time -> time + "\n",
+                false);
+
+        Double initialVelocitySum = initialIterationVelocity.entrySet().stream().reduce(0.0, (acc, entry) -> entry.getValue() + acc, Double::sum);
+
+        IOUtils.CSVWrite(PDF_VELOCITY_INITIAL_FILENAME,
+                initialIterationVelocity.values(),
+                initialVelocitySum / (double) initialIterationVelocity.size() + "\n\n",
+                velocity -> velocity + "\n",
+                false);
+
+        Double thirdIterationVelocitySum = thirdIterationVelocity.entrySet().stream().reduce(0.0, (acc, entry) -> (entry.getValue() / (double) thirdIterations) + acc, Double::sum);
+
+        IOUtils.CSVWrite(PDF_VELOCITY_THIRD_FILENAME,
+                thirdIterationVelocity.values(),
+                thirdIterationVelocitySum / (double) thirdIterationVelocity.size() + "\n\n",
+                velocity -> velocity / (double) thirdIterations + "\n",
+                false);
+
+        IOUtils.CSVWrite(BIG_PARTICLE_TRAJECTORY_FILENAME,
+                bigParticleTrayectory,
+                "",
+                pair -> pair.getKey() + ", " + pair.getValue() + "\n",
+                false);
+    }
+
+    private void saveBigParticlePosition(double simulationTime, double currentTime) {
+        bigParticleTrayectory.add(new Pair<>(bigParticle.getX(), bigParticle.getY()));
+    }
+
+    private void saveVelocities(Map<Long, Double> velocityMap) {
+        for (HardParticle particle : particles) {
+            if (!velocityMap.containsKey(particle.getId())) {
+                velocityMap.put(particle.getId(), particle.getVelocity());
+            } else {
+                Double partialVelocitySum = velocityMap.get(particle.getId());
+                velocityMap.put(particle.getId(), partialVelocitySum + particle.getVelocity());
+            }
+        }
+    }
+
     public static void main(String args[]) {
-        BrownianMotion bm = new BrownianMotion(10);
+        BrownianMotion bm = new BrownianMotion(30);
 
         System.out.println("Starting simulation...");
         long start = System.currentTimeMillis();
 
-        bm.simulate(500);
+        try {
+            bm.simulate(100);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
     }
